b9228cff37858b80b04e53ce1fe8c69d6f93bd35
PesnyaRadugi <mbarazgov02@gmail.com>
2022-11-25 23:34:38 +0300

    Added manual backup
    User can manualy save his progress and load it on his next launch

--- ЯП лаб. №2/Fileservice.py
+++ ЯП лаб. №2/Fileservice.py
@@ -1,9 +1,9 @@
-from functools import partial
+from os import path
 
 
 class Fileservice:
-    def __init__(self):
-        self.data = {}
+    def __init__(self, data):
+        self.data = data
         self.counter = 0
     
     def save_file(self, filename):
@@ -27,6 +27,7 @@ class Fileservice:
     def delete_file(self, id):
         self.data.pop(id)
         self.counter = id
+        print(f'Successfully deleted file with id = {id}')
         
     def change_id(self, old_id, new_id):
         self.data[new_id] = self.data.pop(old_id)
@@ -42,22 +43,44 @@ class Fileservice:
         
     def backup(self, data):
         try:
-            self.data = data
-            self.counter = max(data.keys())
+            save_file = open('save_file.txt', 'w', encoding='utf-8')
+            save_file.write(str(data))
             print('Backup successfull')
         except:
             print('Nothing to save')
             
 def Help():
-        print( '''
+        return ( '''
             save - Save filepath
             get - Get path
             del - Delete filepath
             get_list - Get list of filepaths
             change_id - change id of filepath
-            backup - Make backup
+            backup - Save progress between sessions
             quit - Shup down
         ''')
+
+def Initialize():
+    
+    if path.exists('save_file.txt'):
+        pass
+    else:
+        open('save_file.txt', 'w+', encoding='utf-8')
+    
+    while True:
+        usr_input = input('Would you like to restore your last saved session session ? y/n\n')
+        if usr_input == 'y':
+            save_file = open('save_file.txt', 'r', encoding='utf-8')
+            try:
+                data = eval(save_file.read())
+                return Fileservice(data)
+            except:
+                print('Seems like your save file is empty, creating new.....')
+                return Fileservice({})
+        elif usr_input == 'n':
+            return Fileservice({})
+        else:
+            print('Invalid command try again')
 
 def Navigate_menu(service : Fileservice): 
     while True:
@@ -68,24 +91,23 @@ def Navigate_menu(service : Fileservice):
             case 'get':
                 print(service.get_file(int(input('Enter id\n'))))
             case 'del':
-                print(service.delete_file(int(input("Enter id you'd like to delete\n"))))
+                service.delete_file(int(input("Enter id you'd like to delete\n")))
             case 'get_lst':
-                print(service.get_list([int(i) for i in input('Enter ids separated by space: ').split()]))
+                print(service.get_list([int(i) for i in input('Enter ids separated by space:\n').split()]))
             case 'change_id':
                 service.change_id(int(input('Enter id of file you want to change: ')), int(input('Enter new id: ')))
             case 'backup':
                 service.backup(service.data)
             case 'help':
-                Help()
+                print(Help())
             case 'quit':
                 break
             case _:
                 print('Invalid command, if you stuck use "help" to see list of commands')
                 
 def Main():
-    print('Hello')
-    Navigate_menu(service = Fileservice())
-        
+    print('Hello write "help" to see list of commands')
+    Navigate_menu(Initialize())
     
 if __name__ == '__main__':
     Main()
